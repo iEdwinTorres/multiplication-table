04e9997bbd94280f2922a8bb8f1abffd6c940a86
Edwin Torres <edwin_torres@icloud.com>
2019-11-11 14:02:12 -0500

    Ajusts multiplication table.

--- multiplication-table.js
+++ multiplication-table.js
@@ -1,12 +1,12 @@
 document.write('<span class= "cell header">&times;</span>')
-for (let i = 0; i <= 10; i++) {
+for (let i = 1; i <= 10; i++) {
     document.write('<span class= "cell header">' + i +'</span>')
 }
 document.write('<br>')
 
-for (let x = 0; x <= 10; x++) {
+for (let x = 1; x <= 10; x++) {
     document.write('<span class= "cell header">' + x +'</span>')
-    for (let y = 0 ; y <= 10; y++) {
+    for (let y = 1 ; y <= 10; y++) {
         document.write('<span class= "cell">' + x * y +'</span>')
     }
     document.write('<br>')
